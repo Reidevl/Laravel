ca97cd287e2d16cbc4f11a55f040d6d4119e8847
Reiwil Lugo <reiwillugo@gmail.com>
2023-04-27 11:57:29 -0400

    DDBB and Eloquent

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -8,9 +8,11 @@
 @endsection --}}
 
 
-<x-layouts.app
-    title="Blog"
-    meta-description="Blog meta description">
+<x-layouts.app title="Blog" meta-description="Blog meta description">
 
     <h1>Personal blog</h1>
+    {{-- @dump($posts) --}}
+    @foreach ($posts as $post)
+    <h2> {{ $post->title }} </h2>
+    @endforeach
 </x-layouts.app>
