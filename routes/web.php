a30bbc804b4178b02d0825ba488f0a0a3e72d92e
Reiwil Lugo <reiwillugo@gmail.com>
2023-04-28 22:58:07 -0400

    Register form

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,11 @@
 
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisteredUserController;
 
 
 Route::view('/', 'welcome')->name('home');
 Route::view('/contact', 'contact')->name('contact');
-
-// Route::get('/blog',[PostController::class, 'index'])->name('posts.index');
-// Route::get('/blog/create',[PostController::class, 'create'])->name('posts.create');
-// Route::post('/blog',[PostController::class, 'store'])->name('posts.store');
-// Route::get('/blog/{post}',[PostController::class, 'show'])->name('posts.show');
-// Route::get('/blog/{post}/edit',[PostController::class, 'edit'])->name('posts.edit');
-// Route::patch('/blog/{post}',[PostController::class, 'update'])->name('posts.update');
-// Route::delete('/blog/{post}',[PostController::class, 'destroy'])->name('posts.destroy');
 
 Route::resource('blog', PostController::class, [
     'names'=>'posts',
@@ -25,3 +18,7 @@ Route::view('/about', 'about')->name('about');
 Route::get('/login', function(){
     return 'Login page';
 })->name('login');
+
+
+Route::view('/register', 'auth.register')->name('register');
+Route::post('/register', [RegisteredUserController::class, 'store']);
